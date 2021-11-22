53c21647d31c2ff35b5199ea980568c040169879
HiroUrata <1712026@i.softbank.jp>
2021-11-22 22:17:06 +0900

    各グループにファイルを移動　getDataDetailModel作成

---
+++ RakuAPIAsyncLesson/Model/getDataDetailModel.swift
@@ -0,0 +1,15 @@
+//
+//  getDataDetailModel.swift
+//  RakuAPIAsyncLesson
+//
+//  Created by UrataHiroki on 2021/11/22.
+//
+
+
+struct getDataDetailModel{
+    
+    let mediumImageURL:String?
+    let itemName:String?
+    let itemPrice:String?
+    let itemCaption:String?
+}
